88346e0699d62efa12d3bf84e9c3750c564c8cc1
Lordran <lordran@lordran.dev>
2024-10-09 17:57:46 -0600

    Hough transform and RANSAC method placeholder

---
+++ practices/hough_transform/transform.py
@@ -0,0 +1,12 @@
+
+
+def hough_transform():
+    pass
+
+
+def __run__():
+    pass
+
+
+if __name__ == '__main__':
+    __run__()
---
+++ practices/ransac/ransac.py
@@ -0,0 +1,12 @@
+
+
+def ransac_method():
+    pass
+
+
+def __run__():
+    pass
+
+
+if __name__ == '__main__':
+    __run__()
