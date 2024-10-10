3334eca3b36dbd0fa996c8359dd4bb40facf68d3
Lordran <lordran@lordran.dev>
2024-10-09 18:30:57 -0600

    Hough Transform

--- practices/hough_transform/transform.py
+++ practices/hough_transform/transform.py
@@ -1,11 +1,18 @@
+import cv2
+from numpy import ndarray
+
+import utils.utils as utils
 
 
-def hough_transform():
+def hough_transform(image : ndarray) -> ndarray:
     pass
 
 
-def __run__():
-    pass
+def __run__() -> None:
+    image = cv2.imread('../../img/corn.jpeg')
+    gray_image = utils.img_to_gray(image)
+    cv2.imshow('image', gray_image)
+    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
--- practices/ransac/ransac.py
+++ practices/ransac/ransac.py
@@ -1,3 +1,6 @@
+import cv2
+
+import utils.utils as utils
 
 
 def ransac_method():
@@ -5,7 +8,10 @@ def ransac_method():
 
 
 def __run__():
-    pass
+    image = cv2.imread('../../img/corn.jpeg')
+    gray_image = utils.img_to_gray(image)
+    cv2.imshow('image', gray_image)
+    cv2.waitKey(0)
 
 
 if __name__ == '__main__':
